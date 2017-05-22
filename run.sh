f6ce853bbef461abcfd9f709d8f54eed6f49c2c7
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2017-05-22 13:13:13 +0200

    add xsd validation to run script

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 RULES_DIR="./rules"
+xmllint --noout --schema $RULES_DIR/rules.xsd $RULES_DIR/rules.xml 
 java -jar ./target/solidity-checker-1.0-SNAPSHOT-jar-with-dependencies.jar -p $RULES_DIR/tests -r $RULES_DIR/rules.xml > results.txt
