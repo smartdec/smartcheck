dbfb30a26af3bd613a0ee2295cd9d2233c1c325a
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2017-05-23 13:37:21 +0200

    fixes

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 RULES_DIR="./rules"
-xmllint --noout --schema $RULES_DIR/rules.xsd $RULES_DIR/rules.xml 
-java -jar ./target/solidity-checker-1.0-SNAPSHOT-jar-with-dependencies.jar -p $RULES_DIR/tests -r $RULES_DIR/rules.xml > results.txt
+java -jar ./target/solidity-checker-1.0-SNAPSHOT-jar-with-dependencies.jar -p $RULES_DIR/tests -r $RULES_DIR/rules.xml  > results.txt
