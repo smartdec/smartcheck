e57c809595b88b349867fe7ec2cceaae32870fdd
Chernavskaya <voskresenskaya@smartdec.ru>
2017-09-01 15:14:20 +0300

    solidity_keeping_secrets changed

--- src/test/resources/rules/SOLIDITY_KEEPING_SECRETS.sol
+++ src/test/resources/rules/SOLIDITY_KEEPING_SECRETS.sol
@@ -1,9 +1,12 @@
 contract MarketPlace {
+    uint private data;//a91641
+    uint data;
     function someComp() {
         if (x.balance < 10 && myAddress.balance >= 10) x.transfer(10);
     }
 
-    function kill() private {//a91641
+    function kill() private {
+
         suicide(msg.sender);
     }
 }
