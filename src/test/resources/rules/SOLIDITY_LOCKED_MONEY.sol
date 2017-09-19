d1279095365cc5361bac7dbbb561bb1dc9594bf9
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2017-09-19 15:37:12 +0200

    optimize SOLIDITY_LOCKED_MONEY

--- src/test/resources/rules/SOLIDITY_LOCKED_MONEY.sol
+++ src/test/resources/rules/SOLIDITY_LOCKED_MONEY.sol
@@ -11,14 +11,6 @@ contract GoodMarketPlace2 {
     }
 }
 
-contract GoodMarketPlace3 {
-    function kill() public {
-        suicide(msg.sender);
-    }
-}
-
 contract BadMarketPlace {//a91636
-    function someComp() {
-        if (x.balance < 10 && myAddress.balance >= 10) x.call(10);
-    }
+    function foo() {}
 }
