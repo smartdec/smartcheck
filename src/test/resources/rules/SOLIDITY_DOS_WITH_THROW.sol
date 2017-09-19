d93f07a002c941ae69df59dc8c7d1adae3a3f622
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2017-09-19 14:07:26 +0200

    add pattern 637fdc (SOLIDITY_DOS_WITH_THROW - revert)

--- src/test/resources/rules/SOLIDITY_DOS_WITH_THROW.sol
+++ src/test/resources/rules/SOLIDITY_DOS_WITH_THROW.sol
@@ -3,7 +3,7 @@ contract Auction {
     address addr;
     function bid() payable {
         if (msg.value <= highestBid) { throw; }//a91620
-        if (!currentLeader.send(highestBid)) { throw; } //a91620
+        if (!currentLeader.send(highestBid)) { revert; } //637fdc
 
         for(uint x; x < refundAddresses.length; x++) {//a91621
             addr.transfer(1 wei);
