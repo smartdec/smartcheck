f0446324220274ead263ff2029e70b113c87f60b
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2017-09-19 13:52:32 +0200

    SOLIDITY_DOS_WITH_THROW: simplify tests

--- src/test/resources/rules/SOLIDITY_DOS_WITH_THROW.sol
+++ src/test/resources/rules/SOLIDITY_DOS_WITH_THROW.sol
@@ -1,23 +1,17 @@
 pragma solidity ^0.4.5;
 contract Auction {
+    address addr;
     function bid() payable {
         if (msg.value <= highestBid) { throw; }//a91620
         if (!currentLeader.send(highestBid)) { throw; } //a91620
-    }
-}
-contract SolidityStyleGuideViolation {
-     function refundAll() public {
+
         for(uint x; x < refundAddresses.length; x++) {//a91621
-            if(refundAddresses[x].send(refunds[refundAddresses[x]])) {//a91620
-                throw;
-            }
+            addr.transfer(1 wei);
         }
-    }
-    function refundAll1() public {
+
         while ( x > refundAddresses.length) {//a91622
-            if(refundAddresses[x].send(refunds[refundAddresses[x]])) {//a91620
-                throw;
-            }
+            refundAddresses[x].transfer(1 wei);
         }
+
     }
 }
