88f9c742a0b5a6a09c33fc61d55dce640fc4885e
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2017-09-20 18:32:46 +0200

    SOLIDITY_GAS_LIMIT_AND_LOOPS: merge patterns

--- src/test/resources/rules/SOLIDITY_GAS_LIMIT_AND_LOOPS.sol
+++ src/test/resources/rules/SOLIDITY_GAS_LIMIT_AND_LOOPS.sol
@@ -5,7 +5,7 @@ contract GasLimitAndLoops {
     function foo() {
         while ( x > refundAddresses.length) { //a91626
         }
-        for(bidderIndex = 0; bidderIndex < groups[groupIndex].bidders.length; bidderIndex++) {//a91624
+        for(bidderIndex = 0; bidderIndex < groups[groupIndex].bidders.length; bidderIndex++) {//a91625
         }
         for (uint bidderIndex = 0; bidderIndex < biddersCount; bidderIndex++) {//a91625
         }
