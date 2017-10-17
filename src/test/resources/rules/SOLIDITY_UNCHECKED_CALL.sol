7a8b90a55eed3b742357b84932323a32aca2f0f6
Chernavskaya <voskresenskaya@smartdec.ru>
2017-10-17 13:01:10 +0300

    #17

--- src/test/resources/rules/SOLIDITY_UNCHECKED_CALL.sol
+++ src/test/resources/rules/SOLIDITY_UNCHECKED_CALL.sol
@@ -18,6 +18,14 @@ contract SolidityUncheckedSend {
     function delegatecallSetN(address _e, uint _n) {
         if (!_e.send(bytes(sha3("setN(uint256)"), u))) throw;
     }
+    function delegatecallSetN(address _e, uint _n) {
+     // <yes> <report> SOLIDITY_UNCHECKED_CALL f39eed
+        requre(e.call(bytes(sha3("setN(uint256)"), u)));
+    }
+    function delegatecallSetN(address _e, uint _n) {
+     // <yes> <report> SOLIDITY_UNCHECKED_CALL f39eed
+        assert(_e.send(bytes(sha3("setN(uint256)"), u)));
+}
 }
 
 
