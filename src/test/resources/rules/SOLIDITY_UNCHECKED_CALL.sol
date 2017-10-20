360dd004567e53dff3dab80464f23c8492efd6e0
Ramil <tahaviev@smartdec.ru>
2017-10-20 15:08:59 +0300

    fix solidity tests

--- src/test/resources/rules/SOLIDITY_UNCHECKED_CALL.sol
+++ src/test/resources/rules/SOLIDITY_UNCHECKED_CALL.sol
@@ -1,36 +1,9 @@
 pragma solidity ^0.4.11;
 
 contract SolidityUncheckedSend {
-    function unseatKing(address a, uint w) {
-        // <yes> <report> SOLIDITY_UNCHECKED_CALL f39eed
-        a.call.value(w);
-        // <yes> <report> SOLIDITY_UNCHECKED_CALL f39eed
-        a.send(w);
-        // <yes> <report> SOLIDITY_UNCHECKED_CALL f39eed
-        a.delegatecall(w);
-    }
     function delegatecallSetN(address _e, uint _n) {
-        if (!_e.delegatecall(bytes(sha3("setN(uint256)"), u))) throw;
-    }
-    function delegatecallSetN(address _e, uint _n) {
-        if (!_e.call(bytes(sha3("setN(uint256)"), u))) throw;
-    }
-    function delegatecallSetN(address _e, uint _n) {
-        if (!_e.send(bytes(sha3("setN(uint256)"), u))) throw;
-    }
-    function delegatecallSetN(address _e, uint _n) {
-     // <yes> <report> SOLIDITY_UNCHECKED_CALL f39eed
-        requre(e.call(bytes(sha3("setN(uint256)"), u)));
-    }
-    function delegatecallSetN(address _e, uint _n) {
-     // <yes> <report> SOLIDITY_UNCHECKED_CALL f39eed
-        assert(_e.send(bytes(sha3("setN(uint256)"), u)));
-    }
-    function delegatecallSetN(address _e, uint _n) {
-         // <yes> <report> SOLIDITY_UNCHECKED_CALL a49egh
-            x=_e.send(bytes(sha3("setN(uint256)"), u));
-    }
-
+            if (!_e.delegatecall(bytes(sha3("setN(uint256)"), u))) throw;
+        }
 }
 
 
