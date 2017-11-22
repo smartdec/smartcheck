8cfff1243019530c59fa20dacd57db365872da21
Chernavskaya <voskresenskaya@smartdec.ru>
2017-11-22 15:13:45 +0300

    solidity function returns improved

--- src/test/resources/rules/SOLIDITY_FUNCTIONS_RETURNS_TYPE.sol
+++ src/test/resources/rules/SOLIDITY_FUNCTIONS_RETURNS_TYPE.sol
@@ -1,6 +1,9 @@
 contract f{
 // <yes> <report> SOLIDITY_FUNCTIONS_RETURNS_TYPE 47acc2
     function remainBalanced() public constant returns (uint256){
+        uint a =1000;
+    }
+    function remainBalanced() public constant returns (uint256){
         return balanceOf[this];
     }
     function execute(address _to, uint _value, bytes _data) notNull(_to) onlyOwner external returns (bytes32 _r) {
