7cfb5909983335ec2aa11cd26ebfb5e3d7f6de2a
Chernavskaya <voskresenskaya@smartdec.ru>
2017-11-23 17:36:47 +0300

    solidity 3150 improved

--- src/test/resources/rules/SOLIDITY_VAR_IN_LOOP_FOR.sol
+++ src/test/resources/rules/SOLIDITY_VAR_IN_LOOP_FOR.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.11;
 contract SolidityUncheckedSend {
     function unseatKing(address a, uint w) view returns (uint){
     // <yes> <report> SOLIDITY_VAR_IN_LOOP_FOR f176ab
-        for (var i = 0; i < 256+; i++) {
+        for (var i = 0; i < 257; i++) {
             a=0;
         }
         for (var i = 0; i < 4; i++) {
