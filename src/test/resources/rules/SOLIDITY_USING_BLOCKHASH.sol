48159d3827f6ce29176e62e806d24a3bb8bccd02
Chernavskaya <voskresenskaya@smartdec.ru>
2017-11-29 15:11:46 +0300

    soldity-blockhash-improved

--- src/test/resources/rules/SOLIDITY_USING_BLOCKHASH.sol
+++ src/test/resources/rules/SOLIDITY_USING_BLOCKHASH.sol
@@ -1,8 +1,7 @@
+pragma solidity 0.4.16;
 contract BlockHashTest{
     function getBlockHash(uint64 blockNumber) constant returns (bytes32 blockHash){
-    // <yes> <report> SOLIDITY_USING_BLOCKHASH b629ad
-        d = c.blockhash(blockNumber);
-    // <yes> <report> SOLIDITY_USING_BLOCKHASH b629ad
+// <yes> <report> SOLIDITY_USING_BLOCKHASH b629ad
         return block.blockhash(blockNumber);
     }
 }
