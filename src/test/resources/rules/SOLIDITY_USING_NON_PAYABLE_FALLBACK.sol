a1b7311481a0043fb22aa01ba959d08d70304aba
Chernavskaya <voskresenskaya@smartdec.ru>
2017-12-07 15:14:21 +0300

    fallback

--- src/test/resources/rules/SOLIDITY_USING_NON_PAYABLE_FALLBACK.sol
+++ src/test/resources/rules/SOLIDITY_USING_NON_PAYABLE_FALLBACK.sol
@@ -1,8 +1,31 @@
-contract ERC20Token is StandardToken {
+pragma solidity ^0.4.18;
+contract StandardToken{
 // <yes> <report> SOLIDITY_USING_NON_PAYABLE_FALLBACK 86c34a
     function () {
         throw;
     }
+    function f1() payable {
+        revert();
+    }
+}
+// <yes> <report> SOLIDITY_USING_NON_PAYABLE_FALLBACK 70ac56
+contract ERC20Token is StandardToken {
+    function f() {
+        throw;
+    }
+}
+contract ERC20Token1{
+// <yes> <report> SOLIDITY_USING_NON_PAYABLE_FALLBACK 12fa3c
+    function () payable{
+        throw;
+    }
+}
+contract ERC20Token2{
+    function () payable{
+        if (msg.sender==0x0) throw;
+    }
+}
+contract ERC20Token3{
     function () payable {
         revert();
     }
