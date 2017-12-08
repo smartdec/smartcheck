29016201c01cc574784b9702bebc0698cebbf1e5
Chernavskaya <voskresenskaya@smartdec.ru>
2017-12-08 17:34:25 +0300

    switch of

--- src/test/resources/rules/SOLIDITY_TIMESTAMP_DEPENDENCE.sol
+++ src/test/resources/rules/SOLIDITY_TIMESTAMP_DEPENDENCE.sol
@@ -1,27 +1,27 @@
 contract SolidityStyleGuideViolation {
 
     function doSomething() {
-    // <_yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a
+    /* <no> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a */
         if (now > startTime + week) {
         g.h;
         }
     }
     function doSomething() {
-        // <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE 1955d9 <_yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a
+        // <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE 1955d9 /* <no> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a */
         if ( startTime + week == block.timestamp) {}
-        // <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE 1955d9 <_yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a
+        // <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE 1955d9 /* <no> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a */
         if ( startTime + week == now) {}
         if ( a == b) {}
-        // <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE cd1537 <_yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a
+        // <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE cd1537 /* <no> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a */
         _allocateStakeholdersTokens(totalTokenSupply * p.percentage / percentageDenominator, now + p.vestingPeriod);
-        // <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE cd1537 <_yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a
+        // <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE cd1537 /* <no> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a */
         if (!hasEntry(_account)) {list[_account] = Entry(now, true, listIndex.push(_account) - 1);}
-        // <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE cd1537 <_yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a
+        // <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE cd1537 /* <no> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a */
         time=now+8;
         time=6+7;
-        // <_yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a
+        /* <no> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a */
         require(now >= start && now <= crowdsaleEnd);
-        // <_yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a
+        /* <no> <report> SOLIDITY_TIMESTAMP_DEPENDENCE afb67a */
         require(now > crowdsaleEnd);
     }
 }
@@ -29,16 +29,16 @@ contract SolidityStyleGuideViolation {
 
 contract C {
     function returnsenderbalance() pure returns (uint){
-    // <_yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE  afb67a <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE cd1537
+    /* <no> <report> SOLIDITY_TIMESTAMP_DEPENDENCE  afb67a */ // <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE cd1537
         d=now+1;
     }
     function GVTTeamAllocator() {
-    // <_yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE  afb67a <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE cd1537
+    /* <no> <report> SOLIDITY_TIMESTAMP_DEPENDENCE  afb67a */ // <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE cd1537
             unlockedAt = now + 12 * 30 days;
             owner = msg.sender;
-            // <_yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE  afb67a <yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE cd1537
+            /* <no> <report> SOLIDITY_TIMESTAMP_DEPENDENCE  afb67a */ //<yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE cd1537
             deadline = now + 24 hours;
-            // <_yes> <report> SOLIDITY_TIMESTAMP_DEPENDENCE  afb67a
+            /* <no> <report> SOLIDITY_TIMESTAMP_DEPENDENCE  afb67a */
             if (now > deadline) {
                         if (status == States.Open) {
                             status = States.Closed;
