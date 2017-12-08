82492c9f24c4d712667c3a75cdfa1b7e49e4c12d
Chernavskaya <voskresenskaya@smartdec.ru>
2017-12-08 13:39:52 +0300

    revert

--- src/test/resources/rules/SOLIDITY_REVERT_REQUIRE.sol
+++ src/test/resources/rules/SOLIDITY_REVERT_REQUIRE.sol
@@ -3,6 +3,12 @@ contract f{
     // <yes> <report> SOLIDITY_REVERT_REQUIRE c56b12
         if (x>y) { revert(); }
     }
+    modifier atStage(Stages _stage) {
+        if (stage != _stage)
+            // Contract not in expected state
+            revert();
+        _;
+    }
 }
 contract f{
     function a(){
