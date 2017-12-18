89cd19bee1b11557593ff3c8bfe936333fdf2452
Chernavskaya <voskresenskaya@smartdec.ru>
2017-12-18 13:37:07 +0300

    fallback

--- src/test/resources/rules/SOLIDITY_NO_PAYABLE_FALLBACK.sol
+++ src/test/resources/rules/SOLIDITY_NO_PAYABLE_FALLBACK.sol
@@ -1,44 +1,61 @@
 pragma solidity ^0.4.18;
 
 contract StandardToken{
-// <yes> <report> SOLIDITY_NO_PAYABLE_FALLBACK 86c34a
-    function () {
-        throw;
-    }
-    function f1() payable {
-        return a;
-    }
-}
+ // <yes> <report> SOLIDITY_NO_PAYABLE_FALLBACK 86c34a
+     function () {
+         uint a=1;
+     }
+ }
+contract StandardToken{
 // <yes> <report> SOLIDITY_NO_PAYABLE_FALLBACK 70ac56
-contract ERC20Token is StandardToken {
     function f() {
         throw;
     }
 }
-contract ERC20Token1{
 // <yes> <report> SOLIDITY_NO_PAYABLE_FALLBACK 12fa3c
-    function () payable{
+contract ERC20Token is StandardToken {
+    function () {
+        throw;
+    }
+}
+interface ERC20Token1{
+// <yes> <report> SOLIDITY_NO_PAYABLE_FALLBACK 86c34a
+    function ();
+}
+interface ERC20Token1{
+// <yes> <report> SOLIDITY_NO_PAYABLE_FALLBACK 70ac56
+    function f() payable;
+}
+contract ERC20Token2{
+// <yes> <report> SOLIDITY_NO_PAYABLE_FALLBACK 86c34a
+    function () {
         throw;
     }
 }
 contract ERC20Token2{
-    function () payable{
+// <yes> <report> SOLIDITY_NO_PAYABLE_FALLBACK 12fa3c
+    function () {
+        throw;
+    }
+}
+contract ERC20Token2{
+// <yes> <report> SOLIDITY_NO_PAYABLE_FALLBACK 70ac56
+    function f() payable{
         if (msg.sender==0x0) throw;
     }
 }
-contract ERC20Token3{
-// <yes> <report> SOLIDITY_NO_PAYABLE_FALLBACK 12fa3c
-    function () payable {
-        revert();
+library ERC20Token2{
+    function f() payable{
+        if (msg.sender==0x0) throw;
     }
 }
-library SafeMath {
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-        uint256 c = a * b;
-        assert(c / a == b);
-        return c;
+library ERC20Token2{
+    function f() {
+        if (msg.sender==0x0) throw;
+    }
+}
+library ERC20Token2{
+    function f() {
+        throw;
     }
 }
