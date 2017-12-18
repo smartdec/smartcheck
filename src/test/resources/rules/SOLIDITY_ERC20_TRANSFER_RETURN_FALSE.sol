25c98ecc3228407587f6a513ac7f93535d627ed7
Chernavskaya <voskresenskaya@smartdec.ru>
2017-12-18 20:08:16 +0300

    Merge branch 'verbose_parser_and_lexer' of https://github.com/smartdec/smartcheck into verbose_parser_and_lexer

--- src/test/resources/rules/SOLIDITY_ERC20_TRANSFER_RETURN_FALSE.sol
+++ src/test/resources/rules/SOLIDITY_ERC20_TRANSFER_RETURN_FALSE.sol
@@ -5,13 +5,13 @@ contract TestToken is Token {
     	require(_value > 10 wei);
     	return x;
     }
- <no> <report> SOLIDITY_ERC20_TRANSFER_RETURN_FALSE b180ca
+// <yes> <report> SOLIDITY_ERC20_TRANSFER_RETURN_FALSE b180ca
     function transferFrom(address _spender, uint _value) returns (bool success) {
     	if (_value < 20 wei) throw;
     	return true;
     }
     function transfer(address _spender, uint _value) returns (bool success) {
-    	if (_value < 20 wei) revert;
+    	if (_value < 20 wei) revert();
     	return false;
     }
     function transfer(address _to, uint256 _value) returns (bool success) {
