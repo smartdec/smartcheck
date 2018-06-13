e06067eee0653c189077a02431bf29de5916ed01
Drygin Alexander <DryginAlexander@gmail.com>
2018-06-13 15:08:49 +0300

    Address_hardcoded_rule

--- src/test/resources/rules/SOLIDITY_ADDRESS_HARDCODED.sol
+++ src/test/resources/rules/SOLIDITY_ADDRESS_HARDCODED.sol
@@ -1,78 +1,39 @@
-pragma solidity ^0.4.18;
+pragma solidity 0.4.24;
+
 contract C {
-    event Transfer(address indexed s, uint amount);
-    function g(address s)returns(address){
+    
+    event Transfer(address s, uint amount);
+    
+    function g(address s) returns(address) {
         return s;
     }
-    function returnsenderbalance()  returns (uint){
-    // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  adc165
-        address multisig = 0xf64B584972FE6055a770477670208d737Fff282f;
+    
+    function badPractice() {
         // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  adc165
-        g(0x72ba7d8e73fe8eb666ea66babc8116a41bfb10e2);
+        address x = 0xf64B584972FE6055a770477670208d737Fff282f;
+        // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  adc165
+        x = g(0x72ba7d8e73fe8eb666ea66babc8116a41bfb10e2);
         // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  b140cd
-        address x = 0x123;
+        x = 0x123;
+        // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  f32db1
+        x = address(342);
+        // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  a91b18
+        x = address(0);        
+        // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  a91b18
+        x = g(address(0));
         // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  c67a09
-        address y = 0x0;
-        address myAddress = this;
-        if (msg.sender != 0x0) {
-            revert();
-        }
+        x = 0x0;
+        // <yes> <report> SOLIDITY_ADDRESS_HARDCODED c67a09
+        x = g(0x0);
     }
-    function balanceOf(address _token, address _owner) public constant returns (uint) {
-        uint balance = 0;
-    // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  a91b18
-        if (address(0) == _token) {
-            balance = 12;
-        }
-        else if (address(balance) == _token) {
-            balance = 15;
-    // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  f32db1
-            address s=address(342);
-        }
-        if (address(0) != _token) {
-            balance = 122;
-        }
-        Transfer(0x0, balance);
-    // <yes> <report> SOLIDITY_ADDRESS_HARDCODED c67a09
-        address a = g(0x0);
-        return balance;
-    }
-    modifier onlyWallet() {
-        if (msg.sender != address(this))
-            revert();
-        _;
-    }
-}
-contract SmartToken {
-    event Transfer(address indexed s, uint amount);
-    event Submission(address indexed s, address indexed s1, uint amount);
-    function f1(){
-        uint predecessor=0;
-        Transfer(address(0),predecessor);
-    // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  a91b18
-        Submission(address(0), this, predecessor);
-    }
-}
-contract Token{
+    
+    function gudPractice(address _token, uint balance) {
 
-}
-contract Smart is Token {
-    event Transfer(address indexed s, uint amount);
-    event Submission(address indexed s, address indexed s1, uint amount);
-    function f1(){
-        uint predecessor=0;
-        Transfer(address(0), predecessor);
-    // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  a91b18
-        Submission(address(0), msg.sender, predecessor);
-    }
-}
-contract Smart1{
-    event Transfer(address indexed s, uint amount);
-    event Submission(address indexed s, address indexed s1, uint amount);
-    function f2(){
-    // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  a91b18
-        Transfer(address(0), 4);
-    // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  a91b18
-        Submission(address(0), msg.sender, 4);
+        if (address(0) == _token) or (0x0 == _token) {
+            Transfer(address(0), balance);
+        }
+        if (address(0) != _token) or (0x0 != _token) {
+            Transfer(0x0, balance);
+        }      
     }
 }
