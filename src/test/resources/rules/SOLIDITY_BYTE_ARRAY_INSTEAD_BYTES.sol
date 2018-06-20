0df521a57cf3c4c66bba14707e8b3e60113f969e
Drygin Alexander <DryginAlexander@gmail.com>
2018-06-20 18:04:52 +0300

    Byte_array_rule

--- src/test/resources/rules/SOLIDITY_BYTE_ARRAY_INSTEAD_BYTES.sol
+++ src/test/resources/rules/SOLIDITY_BYTE_ARRAY_INSTEAD_BYTES.sol
@@ -1,8 +1,9 @@
-pragma solidity ^0.4.11;
+pragma solidity 0.4.24;
 contract C {
-    uint someVariable;
+
     // <yes> <report> SOLIDITY_BYTES_BYTE f13a9f
     byte[] someVariable1;
+    
     bytes someVariable2;
     uint[] data;
 }
