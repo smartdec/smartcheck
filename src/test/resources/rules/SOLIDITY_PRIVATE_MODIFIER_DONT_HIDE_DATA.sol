b7c906918cf5f364ac77bc7e38099d6dddac4900
Drygin Alexander <DryginAlexander@gmail.com>
2018-06-21 17:58:43 +0300

    Mistype_fixed

--- src/test/resources/rules/SOLIDITY_PRIVATE_MODIFIER_DONT_HIDE_DATA.sol
+++ src/test/resources/rules/SOLIDITY_PRIVATE_MODIFIER_DONT_HIDE_DATA.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.4.24;
 
 contract MarketPlace {
-    // <yes> <report> PRIVATE_MODIFIER_DONT_HIDE_DATA 5616b2
+    // <yes> <report> SOLIDITY_PRIVATE_MODIFIER_DONT_HIDE_DATA 5616b2
     uint private data1;
     
     uint data2;
