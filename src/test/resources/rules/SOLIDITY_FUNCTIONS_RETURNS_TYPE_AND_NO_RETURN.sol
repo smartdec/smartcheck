964ff74aec6c0da1eabe76273289cb6755e3d421
Drygin Alexander <DryginAlexander@gmail.com>
2018-06-21 16:59:41 +0300

    SOLIDITY_FUNCTIONS_RETURNS_TYPE_AND_NO_RETURN_rule
    new pattern 58bdd3

--- src/test/resources/rules/SOLIDITY_FUNCTIONS_RETURNS_TYPE_AND_NO_RETURN.sol
+++ src/test/resources/rules/SOLIDITY_FUNCTIONS_RETURNS_TYPE_AND_NO_RETURN.sol
@@ -1,27 +1,37 @@
-pragma solidity ^0.4.18;
-contract f{
-    function balanceOf(address who)public view returns (uint256);
-    function allowance(address owner, address spender)public view returns (uint);
-    function remainBalanced() public constant returns (uint256){
-        return balanceOf(this);
-    }
-    function execute(address _to, uint _value, bytes _data) returns (uint256 _r) {
-        _r = balanceOf(_to);
-    }
-    function ResumeEmergencyStop() external  {
-        bool stopped = false;
-    }
-// <yes> <report> SOLIDITY_FUNCTIONS_RETURNS_TYPE_AND_NO_RETURN 47acc2
+pragma solidity 0.4.24;
+
+contract C {
+    // <yes> <report> SOLIDITY_FUNCTIONS_RETURNS_TYPE_AND_NO_RETURN 47acc2
     function remainBalanced1() public constant returns (uint256){
         uint a =1000;
     }
-    function weiLimitOfCurrentStep(uint256 _weiRaised) view public returns(uint256) {
-        uint a =1000;
-        uint FIRST_STEP_UPPER_LIMIT=100;
-        if (a < FIRST_STEP_UPPER_LIMIT) {
-            return FIRST_STEP_UPPER_LIMIT;
-        } else {
-            return 0;
-        }
+    // <yes> <report> SOLIDITY_FUNCTIONS_RETURNS_TYPE_AND_NO_RETURN 58bdd3
+    function execute(address _to, uint _value, bytes _data) returns (uint256 _r) {
+        if (_to == address(0)) {
+            revert();
+        }    
+    }
+    // <yes> <report> SOLIDITY_FUNCTIONS_RETURNS_TYPE_AND_NO_RETURN 58bdd3
+    function execute1(address _to, uint _value, bytes _data) returns (uint256 _r) {
+        _value = _r;
+    }
+    // <yes> <report> SOLIDITY_FUNCTIONS_RETURNS_TYPE_AND_NO_RETURN 58bdd3
+    function execute2(address _to, uint _value, bytes _data) returns (uint256 _r) {
+        _value = balanceOf(address(_r));
+    }
+
+    function balanceOf(address who)public view returns (uint256);
+    
+    function remainBalanced() public constant returns (uint256){
+        return balanceOf(this);
+    }
+
+    function balance(address who)public view returns (uint256 _r);
+
+    function execute3(address _to, uint _value, bytes _data) returns (uint256 _r) {
+        _r = balanceOf(_to);
+    }
+    function execute4(address _to, uint _value, bytes _data) returns (uint256 _r) {
+        return balanceOf(_to);
     }
 }
