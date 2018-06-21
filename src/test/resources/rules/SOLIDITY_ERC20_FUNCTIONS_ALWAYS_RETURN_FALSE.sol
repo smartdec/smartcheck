4f3f54a4e6d2745f3e0eba301cc5c5e8ed44f0db
Drygin Alexander <DryginAlexander@gmail.com>
2018-06-21 13:51:58 +0300

    SOLIDITY_ERC20_FUNCTIONS_ALWAYS_RETURN_FALSE_rule
    instead SOLIDITY_ERC20_TRANSFER_RETURN_FALSE

---
+++ src/test/resources/rules/SOLIDITY_ERC20_FUNCTIONS_ALWAYS_RETURN_FALSE.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.4.24;
+
+contract TestToken {
+    /* <yes> <report> SOLIDITY_ERC20_FUNCTIONS_ALWAYS_RETURN_FALSE b180ca*/
+    function approve(address _spender, uint _value) returns (bool success) {
+    	if (_value < 20 wei) throw;
+    }
+
+    function transferFrom(address _spender, uint _value) returns (bool success) {
+        require(_value > 10 wei);
+        return true;
+    }
+    function transfer(address _spender, uint _value) returns (bool success) {
+    	if (_value < 20 wei) revert();
+    	return false;
+    }
+    function transfer(address _to, uint256 _value) returns (bool success) {}
+ }
