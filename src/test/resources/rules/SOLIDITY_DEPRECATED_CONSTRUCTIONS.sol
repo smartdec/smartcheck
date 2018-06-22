068a05a9f695ca2321c6929f48307e04e13f131d
Drygin Alexander <DryginAlexander@gmail.com>
2018-06-22 15:34:31 +0300

    SOLIDITY_USING_THROW_rule

--- src/test/resources/rules/SOLIDITY_DEPRECATED_CONSTRUCTIONS.sol
+++ src/test/resources/rules/SOLIDITY_DEPRECATED_CONSTRUCTIONS.sol
@@ -9,5 +9,10 @@ contract C {
      // <yes> <report> SOLIDITY_DEPRECATED_CONSTRUCTIONS  187b5a
         return sha3(s);
     }
+    function delegatecallSetN(address _e, uint _n) {
+        // <yes> <report> SOLIDITY_DEPRECATED_CONSTRUCTIONS  49bd2a
+        if (_e != address(0)) throw;
+    }
+}
 
 }
--- src/test/resources/rules/SOLIDITY_USING_THROW.sol
+++
@@ -1,6 +0,0 @@
-contract SolidityUncheckedSend {
-    function delegatecallSetN(address _e, uint _n) {
-        // <yes> <report> SOLIDITY_USING_THROW  49bd2a
-        if (!_e.delegatecall(bytes(sha3("setN(uint256)"), u))) throw;
-    }
-}
