dd38b3b8b3cdc693e52547915f3f542d9adf57b4
Drygin Alexander <DryginAlexander@gmail.com>
2018-07-11 17:43:54 +0300

    Fixed mistype

--- src/test/resources/rules/SOLIDITY_DOS_WITH_REVERT.sol
+++ src/test/resources/rules/SOLIDITY_DOS_WITH_REVERT.sol
@@ -3,7 +3,11 @@ pragma solidity 0.4.24;
 contract Auction {
     address currentLeader;
     uint highestBid;
-    mapping(address => uint) allocated;
+    struct str {
+        uint a;
+        uint b;
+    }
+    mapping(address => str) allocated;
 
     function bid1() payable {
         require(msg.value > highestBid);
@@ -22,13 +26,13 @@ contract Auction {
 
     modifier only_token_holder() {
         // <yes> <report> SOLIDITY_DOS_WITH_REVERT 15ac23
-        require(allocated[msg.sender] > 0 || allocated[msg.sender] > 0);
+        require(allocated[msg.sender].a > 0 || allocated[msg.sender].b > 0);
         _;
     }
 
     modifier only_token_holder2() {
         // <yes> <report> SOLIDITY_DOS_WITH_REVERT 15ac23
-        assert(allocated[msg.sender] > 0 || allocated[msg.sender] > 0);
+        assert(allocated[msg.sender].a > 0 || allocated[msg.sender].b > 0);
         _;
     }
 }
