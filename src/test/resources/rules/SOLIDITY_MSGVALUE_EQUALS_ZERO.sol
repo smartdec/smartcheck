91432a58f127ea40b14e4c5f07050b8594982755
Drygin <drygin@smartdec.ru>
2018-08-31 13:12:10 +0300

    pattern added

---
+++ src/test/resources/rules/SOLIDITY_MSGVALUE_EQUALS_ZERO.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.4.24;
+
+contract MsgValue {
+
+    constructor() public {
+        require(msg.value == 0);
+    }
+
+    function myFunc() public returns(uint) {
+        // <yes> <report> SOLIDITY_MSGVALUE_EQUALS_ZERO 1df89a
+        require(msg.value == 0);
+        // <yes> <report> SOLIDITY_MSGVALUE_EQUALS_ZERO 1df89a
+        if(msg.value == 0) {
+            return(1);
+        }
+        // <yes> <report> SOLIDITY_MSGVALUE_EQUALS_ZERO 1df89a
+        assert(msg.value == 0);
+    }
+
+    function () {
+        // <yes> <report> SOLIDITY_MSGVALUE_EQUALS_ZERO 1df89a
+        require(msg.value == 0);
+    }
+}
