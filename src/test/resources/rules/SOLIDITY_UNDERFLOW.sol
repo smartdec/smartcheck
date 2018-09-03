680783cdf6b8a669cf8adf17bf06720d76997cf8
Drygin <drygin@smartdec.ru>
2018-09-03 15:23:33 +0300

    rename
    SOLIDITY_UNDERFLOW_OVERFLOW -> SOLIDITY_UNDERFLOW

--- src/test/resources/rules/SOLIDITY_UNDERFLOW.sol
+++ src/test/resources/rules/SOLIDITY_UNDERFLOW.sol
@@ -1,22 +1,22 @@
 pragma solidity 0.4.24;
 
-contract UnderOverFlow {
+contract UnderFlow {
     uint a;
 
     function foo_1(){
         uint b;
         int c = 1;
-    // <yes> <report> SOLIDITY_UNDERFLOW_OVERFLOW  11ca45b <yes> <report> SOLIDITY_UNDERFLOW_OVERFLOW  d48ac4
+    // <yes> <report> SOLIDITY_UNDERFLOW  11ca45b <yes> <report> SOLIDITY_UNDERFLOW  d48ac4
         for (uint i=100; i >= 0; i--) {
         }
         for (uint j=0; j <= 0; j--) {
         }
         for (uint k=100; k >= 1; k--) {
         }
-    // <yes> <report> SOLIDITY_UNDERFLOW_OVERFLOW  d48ac4
+    // <yes> <report> SOLIDITY_UNDERFLOW  d48ac4
         require(a>=0);
         require(a<=0);
-    // <yes> <report> SOLIDITY_UNDERFLOW_OVERFLOW  d48ac4
+    // <yes> <report> SOLIDITY_UNDERFLOW  d48ac4
         require(b>=0);
         require(b>=7);
         require(c>=0);
@@ -28,7 +28,7 @@ contract UnderOverFlow {
     }
 
     function foo_2(Mystruct str) internal {
-    // <yes> <report> SOLIDITY_UNDERFLOW_OVERFLOW  d48ac4
+    // <yes> <report> SOLIDITY_UNDERFLOW  d48ac4
         require(str.a1>=0);
         require(str.a2>=0);
     }
@@ -37,7 +37,7 @@ contract UnderOverFlow {
     mapping(address=>int) ibalances;
 
     function foo_3(address user) {
-    // <yes> <report> SOLIDITY_UNDERFLOW_OVERFLOW  733fdd
+    // <yes> <report> SOLIDITY_UNDERFLOW  733fdd
         require(balances[user]>=0);
         require(ibalances[user]>=0);
     }
