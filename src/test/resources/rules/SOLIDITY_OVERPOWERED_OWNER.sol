22d08cf56b2fee1dec877fc958a9e81fe392b0d5
Drygin <drygin@smartdec.ru>
2018-09-14 16:40:55 +0300

    fix

--- src/test/resources/rules/SOLIDITY_OVERPOWERED_OWNER.sol
+++ src/test/resources/rules/SOLIDITY_OVERPOWERED_OWNER.sol
@@ -8,30 +8,35 @@ contract MyTreasure {
         require(msg.sender == myAddr);
         _;
     }
-    // <yes> <report> SOLIDITY_OVERPOWERED_OWNER 43jfq2
-    function withdrawAll() public onlyOwner {
-        myAddr.transfer(this.balance);
-    }
-    // <yes> <report> SOLIDITY_OVERPOWERED_OWNER 43jfq2
-    function transferAll() public {
-        require(msg.sender == myAddr);
-        myAddr.transfer(this.balance);
-    }
-    // <yes> <report> SOLIDITY_OVERPOWERED_OWNER 43jfq2
-    function sendAll() public {
-        require(msg.sender == myAddr);
-        myAddr.transfer(this.balance);
-    }
-    function printAll() public {
-        require(msg.sender == myAddr);
-    }
     // <yes> <report> SOLIDITY_OVERPOWERED_OWNER j83hf7
     function setPrice(uint _price) public onlyOwner {
         price = _price;
     } 
     // <yes> <report> SOLIDITY_OVERPOWERED_OWNER j83hf7
-    function setNewPrice(uint _price) public {
+    function initNewPrice(uint _price) public {
         require(msg.sender == myAddr);
         price = _price;
+    }
+    // <yes> <report> SOLIDITY_OVERPOWERED_OWNER j83hf7
+    function setNewPrice(uint _price) public {
+        require(myAddr == msg.sender);
+        price = _price;
+    }
+
+    function withdrawAll(uint _price) public onlyOwner {
+        myAddr.transfer(this.balance);
+    }
+
+    function setAll() public {
+        require(msg.sender == myAddr);
+        myAddr.transfer(this.balance);
+    }
+
+    function setMy(uint _price) public {
+        myAddr.transfer(this.balance);
+    }
+
+    function setNew() public returns() {
+        require(msg.sender == myAddr);
     }
 }
