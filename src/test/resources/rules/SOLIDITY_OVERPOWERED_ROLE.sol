194921489343d5879feefb080d0848a5f74b9ddb
Drygin <drygin@smartdec.ru>
2018-09-19 15:15:07 +0300

    rule renamed

--- src/test/resources/rules/SOLIDITY_OVERPOWERED_ROLE.sol
+++ src/test/resources/rules/SOLIDITY_OVERPOWERED_ROLE.sol
@@ -8,16 +8,16 @@ contract MyTreasure {
         require(msg.sender == myAddr);
         _;
     }
-    // <yes> <report> SOLIDITY_OVERPOWERED_OWNER j83hf7
+    // <yes> <report> SOLIDITY_OVERPOWERED_ROLE j83hf7
     function setPrice(uint _price) public onlyOwner {
         price = _price;
     } 
-    // <yes> <report> SOLIDITY_OVERPOWERED_OWNER j83hf7
+    // <yes> <report> SOLIDITY_OVERPOWERED_ROLE j83hf7
     function initNewPrice(uint _price) public {
         require(msg.sender == myAddr);
         price = _price;
     }
-    // <yes> <report> SOLIDITY_OVERPOWERED_OWNER j83hf7
+    // <yes> <report> SOLIDITY_OVERPOWERED_ROLE j83hf7
     function setNewPrice(uint _price) public {
         require(myAddr == msg.sender);
         price = _price;
