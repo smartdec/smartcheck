dfa8c556171580b8424dcb737c1389fa13c6160b
Drygin <drygin@smartdec.ru>
2018-10-15 17:00:53 +0300

    rename and fix

--- src/test/resources/rules/SOLIDITY_TRANSFER_IN_LOOP.sol
+++ src/test/resources/rules/SOLIDITY_TRANSFER_IN_LOOP.sol
@@ -6,12 +6,12 @@ contract TransferInCycle {
 
     function dangerousWithdraw() returns (bool) {
         uint l = users.length;
-        // <yes> <report> SOLIDITY_TRANSFER_IN_CYCLE 8jdj43
+        // <yes> <report> SOLIDITY_TRANSFER_IN_LOOP 8jdj43
         for(uint i; i < l; i++) {
             users[i].transfer(balances[users[i]]);
         }
         i=0;
-        // <yes> <report> SOLIDITY_TRANSFER_IN_CYCLE 8jdj43
+        // <yes> <report> SOLIDITY_TRANSFER_IN_LOOP 8jdj43
         while(i < l) {
             users[i].transfer(balances[users[i]]);
             i++;
