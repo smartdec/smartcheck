bbffb647808260b3a35e4679f30c49c86ccd34af
Drygin <drygin@smartdec.ru>
2018-10-16 12:45:32 +0300

    fix

--- src/test/resources/rules/SOLIDITY_TRANSFER_IN_LOOP.sol
+++ src/test/resources/rules/SOLIDITY_TRANSFER_IN_LOOP.sol
@@ -1,5 +1,9 @@
 pragma solidity 0.4.24;
 
+contract ERC20Token {
+    function transfer(address to, uint value) returns(bool);
+}
+
 contract TransferInCycle {
     address[] users;
     mapping(address => uint) balances;
@@ -14,6 +18,15 @@ contract TransferInCycle {
         // <yes> <report> SOLIDITY_TRANSFER_IN_LOOP 8jdj43
         while(i < l) {
             users[i].transfer(balances[users[i]]);
+            i++;
+        }
+    }
+
+    function goodPrictice(address token) {
+        uint l = users.length;
+        uint i;
+        while(i < l) {
+            ERC20Token(token).transfer(users[i], balances[users[i]]);
             i++;
         }
     }
