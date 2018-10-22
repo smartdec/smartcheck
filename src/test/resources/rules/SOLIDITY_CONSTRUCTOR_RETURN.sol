6817e6f12ca70aafc9e7cc495a0b719f69d6a99c
Drygin <drygin@smartdec.ru>
2018-10-22 18:57:22 +0300

    solidity rules, constructor return: add old constructor

--- src/test/resources/rules/SOLIDITY_CONSTRUCTOR_RETURN.sol
+++ src/test/resources/rules/SOLIDITY_CONSTRUCTOR_RETURN.sol
@@ -15,3 +15,19 @@ contract HoneyPot {
         msg.sender.transfer(address(this).balance);
     }
 }
+
+contract HoneyPotOldSchool {
+
+    bytes internal constant ID = hex"60203414600857005B60008080803031335AF100";
+
+    function HoneyPotOldSchool() public payable {
+        bytes memory contract_identifier = ID;
+        // <yes> <report> SOLIDITY_CONSTRUCTOR_RETURN 7gaf21
+        assembly { return(add(0x20, contract_identifier), mload(contract_identifier)) }
+    }
+
+    function withdraw() public payable {
+        require(msg.value >= 1 ether);
+        msg.sender.transfer(address(this).balance);
+    }
+}
