436ad3f55728ab514f3d6cc84e6c3b3bbe313973
Kirill Gorshkov <]jhjDJL1917>
2018-11-19 10:12:57 +0300

    5791_solidity_call_without_data
    call("") is call without data too. This rule is added. Test is upgraded.

--- src/test/resources/rules/SOLIDITY_CALL_WITHOUT_DATA.sol
+++ src/test/resources/rules/SOLIDITY_CALL_WITHOUT_DATA.sol
@@ -21,4 +21,23 @@ contract CallValue {
         if (msg.sender.call.value(1)(3)) {
         }
     }
+    function withdraw5() {
+        // <yes> <report> SOLIDITY_CALL_WITHOUT_DATA 111ttt
+        if (msg.sender.call.value(1)("")) {
+        }
+    }
+    function withdraw6() {
+        // <yes> <report> SOLIDITY_CALL_WITHOUT_DATA 111ttt
+        if (msg.sender.call("")) {
+        }
+    }
+    function withdraw7() {
+        // <yes> <report> SOLIDITY_CALL_WITHOUT_DATA 111ppp
+        if (msg.sender.call.gas(100000)("")) {
+        }
+    }
+    function withdraw8() {
+        if (msg.sender.call.value(1)(" ")) {
+        }
+    }
 }
